1228b921a7fbd1d72b2468b68ba85c21c893c718
LeeKwanHyun <zisu55@naver.com>
2021-10-29 14:06:55 +0900

    Category: Title
    Description:
    
    Type:
    
    Note:
        - N/A
        - N/A

---
+++ assets/js/make.js
@@ -0,0 +1,67 @@
+function makeJava(){
+    window.location.href = 'makeJava.html'
+}
+
+function moveJava(){
+    window.location.href = 'postsJava.html'
+}
+
+
+
+function makeCss(){
+    window.location.href = 'makeCss.html'
+}
+
+function moveCss(){
+    window.location.href = 'postsCss.html'
+}
+
+
+
+function makeHtml(){
+    window.location.href = 'makeHtml.html'
+}
+
+function moveHtml(){
+    window.location.href = 'postsHtml.html'
+}
+
+
+
+function makeKotlin(){
+    window.location.href = 'makeKotlin.html'
+}
+
+function moveKotlin(){
+    window.location.href = 'postsKotlin.html'
+}
+
+
+
+function makeMysql(){
+    window.location.href = 'makeMysql.html'
+}
+
+function moveMysql(){
+    window.location.href = 'postsMysql.html'
+}
+
+
+
+function makePhp(){
+    window.location.href = 'makePhp.html'
+}
+
+function movePhp(){
+    window.location.href = 'postsPhp.html'
+}
+
+
+
+function makeXml(){
+    window.location.href = 'makeXml.html'
+}
+
+function moveXml(){
+    window.location.href = 'postsXml.html'
+}
